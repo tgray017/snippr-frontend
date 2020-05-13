1573d6379da08d413e6bc3b74d29ba01cd6ad07e
Thomas Gray <thomas.gray@tgray.local>
2020-05-12 21:45:42 -0400

    Get podcast results to display thumnail image

--- src/components/SearchPodcastsResults.js
+++ src/components/SearchPodcastsResults.js
@@ -3,6 +3,10 @@ import Card from 'react-bootstrap/Card'
 import Moment from 'react-moment'
 import TextTruncate from 'react-text-truncate'
 import { AnimationWrapper } from 'react-hover-animation'
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 
 export default class SearchPodcastsResults extends Component {
@@ -18,23 +22,32 @@ export default class SearchPodcastsResults extends Component {
       animationConfig='wobbly'
       >
         <Card>
-          <Card.Body>
-            <Card.Title>{this.props.title}</Card.Title>
-            <Card.Text>
-              <TextTruncate
-                line={2}
-                truncateText="…"
-                text={this.props.description}
-              />
-            </Card.Text>
-            <Card.Subtitle className="mb-2 text-muted">
-              {'Last aired '}
-              <Moment
-                format="MMM Do, YYYY">
-                {this.props.lastAirDate}
-              </Moment>
-            </Card.Subtitle>
-          </Card.Body>
+          <Container>
+            <Row className="align-items-center">
+              <Col>
+                <Image src={this.props.image} thumbnail/>
+              </Col>
+              <Col xs={10}>
+                <Card.Body>
+                  <Card.Title>{this.props.title}</Card.Title>
+                  <Card.Text>
+                    <TextTruncate
+                      line={2}
+                      truncateText="…"
+                      text={this.props.description}
+                    />
+                  </Card.Text>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    {'Last aired '}
+                    <Moment
+                      format="MMM Do, YYYY">
+                      {this.props.lastAirDate}
+                    </Moment>
+                  </Card.Subtitle>
+                </Card.Body>
+              </Col>
+            </Row>
+          </Container>
         </Card>
       </AnimationWrapper>
     )
