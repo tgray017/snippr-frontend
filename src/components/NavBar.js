3bf85ca1513801e3eb0f75659c7c7dee8cf9bc75
Thomas Gray <thomas.gray@dhcp-10-154-96-63.vpn.oracle.com>
2020-09-26 12:09:54 -0400

    Refactor Navbar and prevent reload on click

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,45 +1,49 @@
 import React, { PureComponent } from 'react'
+import { Link } from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 
 export default class NavBar extends PureComponent {
 
-  render() {
+  renderLogo = () => {
     let logo = require('../images/snippr-logo-gray.svg')
-    if(this.props.authenticated) {
+    return (
+      <Navbar.Brand as={Link} to="/">
+        <img
+          src={logo}
+          width="60"
+          alt="snippr logo"
+        />
+      </Navbar.Brand>
+    )
+  }
+
+  renderNavLinks = () => {
+    if (this.props.authenticated) {
       return (
-        <Navbar bg="dark" variant="dark" sticky="top" className="navbar-snippr">
-          <Navbar.Brand href="/">
-            <img
-              src={logo}
-              width="60"
-              alt="snippr logo"
-            />
-          </Navbar.Brand>
-          <Nav>
-            <Nav.Link className="pt-0" href="/search" eventKey="/search">search</Nav.Link>
-            <Nav.Link className="pt-0" href="/library" eventKey="/library">library</Nav.Link>
-            <Nav.Link className="pt-0" href="/logout" eventKey="/logout">logout</Nav.Link>
-          </Nav>
-        </Navbar>
+        <Nav>
+          <Nav.Link as={Link} className="pt-0" to="/search" eventKey="/search">search</Nav.Link>
+          <Nav.Link as={Link} className="pt-0" to="/library" eventKey="/library">library</Nav.Link>
+          <Nav.Link as={Link} className="pt-0" to="/logout" eventKey="/logout">logout</Nav.Link>
+        </Nav>
       )
     } else {
       return (
-        <Navbar bg="dark" variant="dark" sticky="top">
-          <Navbar.Brand href="/">
-            <img
-              src={logo}
-              width="60"
-              alt="snippr logo"
-            />
-          </Navbar.Brand>
-          <Nav>
-            <Nav.Link className="pt-0" href="/search" eventKey="/search">search</Nav.Link>
-            <Nav.Link className="pt-0" href="/login" eventKey="/login">login</Nav.Link>
-            <Nav.Link className="pt-0" href="/signup" eventKey="/signup">signup</Nav.Link>
-          </Nav>
-        </Navbar>
+        <Nav>
+          <Nav.Link as={Link} className="pt-0" to="/search" eventKey="/search">search</Nav.Link>
+          <Nav.Link as={Link} className="pt-0" to="/login" eventKey="/login">login</Nav.Link>
+          <Nav.Link as={Link} className="pt-0" to="/signup" eventKey="/signup">signup</Nav.Link>
+        </Nav>
       )
     }
+  }
+
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark" sticky="top" className="navbar-snippr">
+        {this.renderLogo()}
+        {this.renderNavLinks()}
+      </Navbar>
+    )
   }
 }
